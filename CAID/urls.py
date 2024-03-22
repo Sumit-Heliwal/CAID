c6391577215cee2f1c31d174dcd6418e30169759
Sumit Heliwal <47483109+Sumit-Heliwal@users.noreply.github.com>
2024-03-22 16:02:52 +0530

    22/03/24

--- CAID/urls.py
+++ CAID/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
     
 """
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path,include
@@ -25,11 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls)
     
 
-]
-
-
-from django.conf import settings
-from django.conf.urls.static import static
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Personal_Data/urls.py
+++ Personal_Data/urls.py
@@ -1,6 +1,8 @@
 from turtle import home
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -14,4 +16,8 @@ urlpatterns = [
     # path("contact/", views.contact, name="contact"),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
